611d9f63d7bbf942b22592db76a3e083642edb7f
duyabo <duyabo@hundun.cn>
2021-01-27 14:43:02 +0800

    注释

--- utils/auth_check_plus.py
+++ utils/auth_check_plus.py
@@ -19,11 +19,20 @@ ALGORITHM_SIGN = 'HS256'
 UNIQUE_EXP_KEY = 'unique:exp:{exp}'
 SECRET_UID_KEY = 'secret:uid:{user_id}'
 
-# 下面是限流方案。
-# 滑动窗口算法实现，更精准，但也更复杂，姑且不考虑。如有意，可以加多个限流标准：比如现在建立的是分钟级别的，可以增加十分钟级别的，半小时级别的...。
-# nginx ngx_http_limit_req_module 的 ip+url 进行频率限制，粒度更细，对资源保护更好，建议对无 sign 接口设置。
-SUCCESS_CNT_ONE_MINUTE = 'success_cnt:one_minute:{user_id}'
+# 下面是限流方案。滑动窗口算法实现，更精准，但也更复杂，姑且不考虑。如有意，可以加多个限流标准：比如现在建立的是分钟级别的，可以增加十分钟级别的，半小时级别的...。
+# nginx ngx_http_limit_req_module 进行频率限制，也可以应用程序结合 redis 进行频率监控。
+# 后者对嫌疑请求的处理更灵活（比如出滑块，返回假数据，返回错误提示等），前者对恶意 ip 或关键 url 进行保护（直接拒绝响应）。
+# 一般应用程序内部滑块处理：
+# 0，什么都不可信：不会有人知道这个接口？不会有人知道加密算法？不会有人无聊到压测这个接口？不会有人想获取这个接口到返回数据？no。所以，尽可能多的接口使用 sign 验证。
+# 1，无 sign 接口，对 ip 进行频次监控并处理。
+# 2，有 sign 接口，只需要 uid 进行频次监控并处理。
+# 3，对关键接口保护，比如读写关键数据的接口，比如资源消耗严重的接口，在1，2的基础上，增加对于指定 url 的监控和处理。
+# 4，一般 1/2 对触发频次阈值非常宽松，3 可以适当严格。
+# 5, 滑块验证通过后，可以对请求放行，并对频次清零重新计数监控，做到对正常用户的最小影响。
+# 6，尽可能实时的自动化的维护一个 ip 黑名单，以及一个 uid 黑名单，并在应用中对这些 ip/uid 发出的请求进行处理。
+# 7，如果 6 中黑名单请求量特别大，已经影响到系统正常提供服务，就直接在 ng 进行 503 拒绝响应。
 FAILED_CNT_ONE_MINUTE = 'failed_cnt:one_minute:{remote_ip}'
+SUCCESS_CNT_ONE_MINUTE = 'success_cnt:one_minute:{user_id}'
 SUCCESS_CNT_LIMIT_ONE_MINUTE = 300
 FAILED_CNT_LIMIT_ONE_MINUTE = 10
 
