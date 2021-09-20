478a73febaf0c541a51310ab8f3800c201f83678
duyabo <duyabo@hundun.cn>
2021-09-20 20:42:41 +0800

    红黑树插入

--- utils/red_black.py
+++ utils/red_black.py
@@ -17,25 +17,65 @@ class RBNode(object):
         self.left = left
         self.right = right
 
+    def update(self, color=None, key=None, value=None, parent=None, left=None, right=None):
+        if color is not None:
+            self.color = color
+        if key is not None:
+            self.key = key
+        if value is not None:
+            self.value = value
+        if parent is not None:
+            self.parent = parent
+        if left is not None:
+            self.left = left
+        if right is not None:
+            self.right = right
+
+    def set_leaf(self):
+        left = RBNode()
+        left.parent = self
+        self.left = left
+
+        right = RBNode()
+        right.parent = self
+        self.right = right
+
 
 class RBTree(object):
-    def __init__(self, root_node=None):
+    def __init__(self, root_node):
         self.root = root_node
+
+    @classmethod
+    def build(cls):
+        node = RBNode()
+        node.set_leaf()
+        return cls(node)
 
     def search(self, key):
         if self.root is None:  # 空红黑树
-            return None
+            self.root = RBTree.build().root
+            return self.root
         if self.root.key is None:  # 叶子节点
-            return None
+            return self.root
         if self.root.key == key:
-            return self.root.value
+            return self.root
         if self.root.key > key:
             return RBTree(self.root.left).search(key)
         if self.root.key < key:
             return RBTree(self.root.right).search(key)
 
-    def insert(self, node):
+    def revolve(self):
+        """todo 最小子树，需要旋转保持平衡"""
         pass
+
+    def insert(self, node):
+        target_node = self.search(node.key)
+        if target_node.key == node.key:
+            target_node.update(value=node.value)
+        else:  # 叶子节点
+            node.set_leaf()
+            target_node.update(color=COLOR_RED, key=node.key, value=node.value, left=node.left, right=node.left)
+            RBTree(target_node.parent).revolve()
 
     def delete(self, key):
         pass
